955da2897bf97c86b74ae4f4117bc8dac98d317e
Ondřej Kutáček <kutaceko22@spstrutnov.cz>
2024-12-09 13:35:13 +0100

    Kalkulačka hotova

--- server.js
+++ server.js
@@ -1,34 +1,50 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
-let buttonPressCounts = {
-    '1': 0,
-    '2': 0,
-    '3': 0,
-    '4': 0,
-    '5': 0,
-    '6': 0,
-    '7': 0,
-    '8': 0,
-    '9': 0,
-    '0': 0,
-    '+': 0,
-    '-': 0,
-    '*': 0,
-    '/': 0,
-    '=': 0,
-    'C': 0,
-    'CE': 0,
-    'Del': 0,
-    '%': 0,
-    '1/x': 0,
-    'x²': 0,
-    '√': 0,
-    '±': 0,
-    '.': 0
-};
+let buttonPressCounts = {};
+
+// Load button press counts from JSON file
+function loadButtonPressCounts() {
+    if (fs.existsSync('buttonPressCounts.json')) {
+        const data = fs.readFileSync('buttonPressCounts.json');
+        buttonPressCounts = JSON.parse(data);
+    } else {
+        buttonPressCounts = {
+            '1': 0,
+            '2': 0,
+            '3': 0,
+            '4': 0,
+            '5': 0,
+            '6': 0,
+            '7': 0,
+            '8': 0,
+            '9': 0,
+            '0': 0,
+            '+': 0,
+            '-': 0,
+            '*': 0,
+            '/': 0,
+            '=': 0,
+            'C': 0,
+            'CE': 0,
+            'Del': 0,
+            '%': 0,
+            '1/x': 0,
+            'x²': 0,
+            '√': 0,
+            '±': 0,
+            '.': 0
+        };
+    }
+}
+
+
+function saveButtonPressCounts() {
+    fs.writeFileSync('buttonPressCounts.json', JSON.stringify(buttonPressCounts, null, 2));
+}
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
@@ -37,6 +53,7 @@ app.post('/button-press', (req, res) => {
     const { button } = req.body;
     if (buttonPressCounts.hasOwnProperty(button)) {
         buttonPressCounts[button]++;
+        saveButtonPressCounts();
     }
     res.json(buttonPressCounts);
 });
@@ -46,5 +63,6 @@ app.get('/button-press-counts', (req, res) => {
 });
 
 app.listen(port, () => {
+    loadButtonPressCounts();
     console.log(`Server running at http://localhost:${port}/`);
 });
